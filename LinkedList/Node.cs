bcd6bacf8504ac62e620f0b0f22176f79d39e706
AkshayKumarTN <tnak369@gmail.com>
2021-07-09 16:06:31 +0530

    [Akshay] initial Commit

---
+++ LinkedList/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class Node
+    {
+        int data;
+        Node Next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
